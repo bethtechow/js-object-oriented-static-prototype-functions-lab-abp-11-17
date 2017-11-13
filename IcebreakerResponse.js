ce907f8bc6b86085ff7b62e117d3239f28e9309d
bethtechow <bethtechow@yahoo.com>
2017-11-13 02:39:24 +0000

    automatically backed up by learn

--- IcebreakerResponse.js
+++ IcebreakerResponse.js
@@ -1,4 +1,8 @@
 function IcebreakerResponse(email){
   this.email = email
 }
-
+User.BatchCreate = function(data){
+  return data.map(function(userData){
+    return new User(userData[0], userData[1])
+  })
+}
